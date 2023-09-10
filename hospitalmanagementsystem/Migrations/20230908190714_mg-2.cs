f7001c348293a8b3fd54923071cf144f5097b787
G.Kirthana <kirthanakiro74@gmail.com>
2023-09-10 22:18:51 +0530

    Add project files.

---
+++ hospitalmanagementsystem/Migrations/20230908190714_mg-2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hospitalmanagementsystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AspNetUsers");
+        }
+    }
+}
